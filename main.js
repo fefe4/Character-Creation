f968735e3b8d293beaea53001a18118c3c52781a
Fefe <mynameisfefe@protonmail.com>
2021-02-06 01:36:57 -0300

    fixed img names

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 let currentSlide = 0;
 const slides = document.getElementsByClassName("slides");
-function displaySlides(n) {  
-  console.log (n);
-  const slides = document.getElementsByClassName("slides");  
-  for (let i=0; i<slides.length; i++) {
-    if (i == n) {slides[i].style.display = "block";}
-    else {slides[i].style.display = "none";}
-  };
-}  
-displaySlides (0)
+function displaySlides(n) {
+  console.log(n);
+  const slides = document.getElementsByClassName("slides");
+  for (let i = 0; i < slides.length; i++) {
+    if (i == n) {
+      slides[i].style.display = "block";
+    } else {
+      slides[i].style.display = "none";
+    }
+  }
+}
+displaySlides(0);
 
 const left = document.getElementById("left");
-left.addEventListener ('click', () =>{
+left.addEventListener("click", () => {
   currentSlide -= 1;
-  if (currentSlide < 0) {currentSlide = slides.length-1;}
-  displaySlides (currentSlide);
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  displaySlides(currentSlide);
 });
 
 const right = document.getElementById("right");
-right.addEventListener ('click', () =>{
+function sliderright() {
   currentSlide += 1;
-  if (currentSlide > slides.length-1) {currentSlide = 0;}
-  displaySlides (currentSlide);
+  if (currentSlide > slides.length - 1) {
+    currentSlide = 0;
+  }
+  displaySlides(currentSlide);
+}
+right.addEventListener("click", () => {
+  sliderright();
 });
 
+setInterval(sliderright, 5000);
